6bf08eafc86e24bad1fbbbe6fe413376992db36c
Freddy Campos <freddycampos1994@gmail.com>
2022-02-05 15:49:58 -0400

    Added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+import NavbarContainer from "@material-tailwind/react/NavbarContainer";
+import NavbarWrapper from "@material-tailwind/react/NavbarWrapper";
+import NavbarToggler from "@material-tailwind/react/NavbarToggler";
+import NavbarCollapse from "@material-tailwind/react/NavbarCollapse";
+import Nav from "@material-tailwind/react/Nav";
+import NavLink from "@material-tailwind/react/NavLink";
+
+import { motion } from "framer-motion";
+
+import logo from "../assets/static/logo.png";
+
+const Header = () => {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  return (
+    <>
+      <div
+        className="flex flex-wrap items-center justify-between py-2.5 px-3 mb-3 false undefined"
+        style={{ backgroundColor: "#3F3F3F" }}
+      >
+        <NavbarContainer>
+          <NavbarWrapper>
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <img src={logo} alt="logo" style={{ width: "40px" }} />
+            </motion.div>
+            <NavbarToggler
+              color="white"
+              onClick={() => setOpenNavbar(!openNavbar)}
+              ripple="light"
+            />
+          </NavbarWrapper>
+
+          <NavbarCollapse open={openNavbar}>
+            <Nav>
+              <motion.div
+                whileHover={{ scale: 1.05, backgroundColor: "rgba(0,0,0,0.2)" }}
+              >
+                <NavLink href="#about-me" ripple="light">
+                  Sobre mí
+                </NavLink>
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.05, backgroundColor: "rgba(0,0,0,0.2)" }}
+              >
+                <NavLink href="#credentials" ripple="light">
+                  Credenciales
+                </NavLink>
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.05, backgroundColor: "rgba(0,0,0,0.2)" }}
+              >
+                <NavLink href="#contact" ripple="light">
+                  Contáctame
+                </NavLink>
+              </motion.div>
+            </Nav>
+          </NavbarCollapse>
+        </NavbarContainer>
+      </div>
+    </>
+  );
+};
+
+export default Header;
