489a4003a1b9c3e88cc16b6684bb5a4077631a07
Freddy Campos <freddycampos1994@gmail.com>
2022-02-22 13:17:48 -0400

    Added certifications

--- src/documents/certifications.js
+++ src/documents/certifications.js
@@ -1,8 +1,56 @@
+import nodejs from "../assets/certificates/NodeJS.jpg";
+import nodejs2 from "../assets/certificates/nodejs-mongo-websockets.jpg";
+import ExpressJS from "../assets/certificates/ExpressJs.jpg";
+import ReactJS from "../assets/certificates/ReactJs.jpg";
+import ReactN from "../assets/certificates/React-native-listas-apis.jpg";
+import JavaScript from "../assets/certificates/Javascript-profesional.jpg";
+import Poo from "../assets/certificates/Poo.jpg";
+import Web3Js from "../assets/certificates/Web3JS.jpg";
+import Solidity from "../assets/certificates/Solidity.jpg";
+import SC from "../assets/certificates/Smart-contracts.jpg";
+import Ethereum from "../assets/certificates/Ethereum-developers.jpg";
+import Frontend from "../assets/certificates/Frontend-developer.jpg";
+import Python from "../assets/certificates/Python.jpg";
+import Flask from "../assets/certificates/Flask.jpg";
+import Docker from "../assets/certificates/docker.jpg";
+import SQL from "../assets/certificates/sql-mysql.jpg";
 import gitGithub from "../assets/certificates/git-github.jpg";
 import backendDjango from "../assets/certificates/backend-python.jpg";
 import bd from "../assets/certificates/bd.jpg";
 
 export const certificactions = [
+  {
+    name: "Node JS",
+    description: "Backend con Node Js",
+    ref: "https://drive.google.com/file/d/1jOXc-WR4O6QPeT87kKBKzGjRgVCCIv3y/view?usp=sharing",
+    image: nodejs,
+  },
+  {
+    name: "NodeJS y MongoDB",
+    description:
+      "Curso de Node JS, base de datos con MongoDB y uso de Websockets",
+    ref: "https://drive.google.com/file/d/1L8Pwt607SQURcRxuXGVFRD4i8p224dBZ/view?usp=sharing",
+    image: nodejs2,
+  },
+  {
+    name: "ExpressJS",
+    description: "Curso de Express JS",
+    ref: "https://drive.google.com/file/d/1FwvP1bx6tk9KbIxYJzDYuHQy69ePYCL_/view?usp=sharing",
+    image: ExpressJS,
+  },
+  {
+    name: "React JS",
+    description: "Curso practico de React JS",
+    ref: "https://drive.google.com/file/d/1kl4q_puPUFlWw1qwnsnTpe7roQYovGMj/view?usp=sharing",
+    image: ReactJS,
+  },
+  {
+    name: "React Native",
+    description:
+      "Curso practico de React Native, manejo de listas y consumo de APIS",
+    ref: "https://drive.google.com/file/d/1krzWQcMH8VTC4t1zkJZSF4G8ZuLZGpto/view?usp=sharing",
+    image: ReactN,
+  },
   {
     name: "Git y Github",
     description: "Curso profesional de git y github",
@@ -10,15 +58,84 @@ export const certificactions = [
     image: gitGithub,
   },
   {
+    name: "Profesional de Javascript",
+    description: "Curso profesional de JavaScript",
+    ref: "https://drive.google.com/file/d/1A92dkZ6nsw985abOx18uYVSma75IiMSZ/view?usp=sharing",
+    image: JavaScript,
+  },
+  {
+    name: "Programación orientada a objetos",
+    description: "Curso de programación orientada a objetos",
+    ref: "https://drive.google.com/file/d/1y1TUYExzyaS2i9B2XL4V5JMohnm2rkFt/view?usp=sharing",
+    image: Poo,
+  },
+  {
+    name: "DAPPS",
+    description:
+      "Curso de desarrollo frontend de aplicaciones descentralizadas con Web3JS",
+    ref: "https://drive.google.com/file/d/1YgE_WmO_Xy5ZYkgoqSV-hV4vmmWJLlpk/view?usp=sharing",
+    image: Web3Js,
+  },
+  {
+    name: "Introducción a Solidity",
+    description: "Curso de introducción a solidity",
+    ref: "https://drive.google.com/file/d/1SMew5OZuQ5J1WRTO-Jne_KQhOmF7nSSE/view?usp=sharing",
+    image: Solidity,
+  },
+  {
+    name: "Contratos Inteligentes",
+    description:
+      "Curso de introducción al desarrollo de blockchain, contratos inteligentes",
+    ref: "https://drive.google.com/file/d/1vvkNJVj0TWMBHcZv9UlhHvz-2OOXVtfP/view?usp=sharing",
+    image: SC,
+  },
+  {
+    name: "Ethereum for developers",
+    description: "Curso de Ethereum para developers",
+    ref: "https://drive.google.com/file/d/11BA0YvIo69Egy8n8fx4nTWwBI4SGoYjh/view?usp=sharing",
+    image: Ethereum,
+  },
+  {
+    name: "Frontend Developer",
+    description: "Curso practico de frontend developer",
+    ref: "https://drive.google.com/file/d/1eHMldAd-UIuYxJwsJ45wKLXwzjIgsEA_/view?usp=sharing",
+    image: Frontend,
+  },
+
+  {
     name: "Backend Django",
     description: "Desarrollo backend con python y Django",
     ref: "https://drive.google.com/file/d/1gp2XQK-o1sjGKBezpvYzal0MkbMaVt3-/view?usp=sharing",
     image: backendDjango,
   },
   {
+    name: "Python, Html, Css, MySql y Flask",
+    description: "Curso en linea de Python, HTML, CSS, Flask y MySql",
+    ref: "https://drive.google.com/file/d/1byOraYPcKV7V0_Q3zu0kqFHgQRWfq5O1/view?usp=sharing",
+    image: Python,
+  },
+  {
+    name: "Flask",
+    description: "Curso de Flask",
+    ref: "https://drive.google.com/file/d/13OzNH5blJCPm8mHu-l9xIQfKT-SFvxp4/view?usp=sharing",
+    image: Flask,
+  },
+  {
     name: "Fundamentos de bases de datos",
     description: "Curso de fundamentos de bases de datos",
     ref: "https://drive.google.com/file/d/1XZQuOf_d3WeAZsemY2Og-hF7mK0P5hXl/view?usp=sharing",
     image: bd,
+  },
+  {
+    name: "SQL y MySql",
+    description: "Curso de SQL y MySql",
+    ref: "https://drive.google.com/file/d/1ZwQwY7vSGhTYVeZ9mIqVPTuwojTyb5KP/view?usp=sharing",
+    image: SQL,
+  },
+  {
+    name: "Docker",
+    description: "Curso de Docker",
+    ref: "https://drive.google.com/file/d/1lJfEHChhlyYkf2XneKMKlQ_5uNpqa_Nr/view?usp=sharing",
+    image: Docker,
   },
 ];
